6c63104fb2629e9aa31e4046d9c2dd73cf9b8b10
Murillo Mesquita <mu.pracucio@gmail.com>
2021-07-21 00:06:27 -0300

    Ouvindo e salvando eventos do stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -47,8 +47,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
       customerId = stripeCustomer.id
     }
-
-    
     
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: customerId,
